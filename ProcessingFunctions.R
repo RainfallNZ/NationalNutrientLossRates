c1e562e320417b4ee6578af4d4437540ce5b57ad
rainfallnz <tim.kerr@rainfall.nz>
2022-06-07 09:51:59 +1200

    Major updates as the majority of the process is implemented

--- ProcessingFunctions.R
+++ ProcessingFunctions.R
@@ -47,14 +47,31 @@ CatchmentCreator <- function(Watersheds=RECV2Watersheds,ReachNetwork=RECV2ReachN
   return(Catchment)
 }
 
-#' A function to reclassify LCDBV5
+#' A function to do a QGIS Union in R
 #'
-#'LCDBV5Reclassify() creates a vector of the reach ID numbers (nzsegment, nzsgmnt or nzseg_v3 attribute) downstream of a reach. It uses the REC V2.4, or Environment Southland's REC3 network. See \href{https://niwa.co.nz/freshwater-and-estuaries/management-tools/river-environment-classification-0}{NIWA REC V2} for details about REC V2.
-#'@param LCDBV5 A simple features (sf)  polygon representation of the LCDBV5.
-#'@parma ReclassTable A dataframe of the RECV2 database, with at least the nzsegment, to_node and from_node attributes
-#'@param SourceReach The reach number, i.e. nzsegment attribute, of the reach for which the catchment is required
-#'@author Tim Kerr, \email{Tim.Kerr@@Rainfall.NZ}
-#'@return A simple feature (sf) polygon of the catchment
-#'@keywords REC River Environment Classification
+#'GISunion() carries out a union as implemented in QGIS and ArcGIS. The function is stolen from this
+#' \href{https://stackoverflow.com/questions/54710574/how-to-do-a-full-union-with-the-r-package-sf}{StackOverflow answer}
+#' The R st_union function and st_combine do something but it's not quite right  
+#'@param a A simple features object to be unioned
+#'@parma b Another simple features object to be unioned
+#'@author Torsten Berg
+#'@return A simple feature (sf) object
 #'@export
-LCDBV5Reclassify <- function(LCDBV5 = LCDBV5, ReclassTable = )
+  GISunion <- function(a,b) {
+    #Check that the same feature types are being unioned
+    #Multipolygons won't union with polygons in this function
+    stopifnot(st_geometry_type(a) == st_geometry_type(b))
+    
+    #Make sure the geometry name attribute is the same for both objects otherwise the
+    #rbind.fill function used later on won't bind the geometries togehter
+    GeometryNameOf_a <- attr(a, "sf_column")
+    if (attr(b, "sf_column") != GeometryNameOf_a) st_geometry(b) =GeometryNameOf_a
+    
+    st_agr(a) = "constant"
+    st_agr(b) = "constant"
+    op1 <- st_difference(a,st_union(b))
+    op2 <- st_difference(b, st_union(a))
+    op3 <- st_intersection(b, a)
+    union <- rbind.fill(op1, op2, op3)
+    return(st_as_sf(union))
+  }
